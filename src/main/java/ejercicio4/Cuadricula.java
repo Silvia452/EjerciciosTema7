bb38e388a493177f0479cbcc54f5bd51a0127909
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-04 16:47:17 +0100

    Todos los ejercicios funcionan

--- src/main/java/ejercicio4/Cuadricula.java
+++ src/main/java/ejercicio4/Cuadricula.java
@@ -1,4 +1,61 @@
 package ejercicio4;
 
 public class Cuadricula {
+
+    private int FILAS = 10;
+    private int COLUMNAS = 10;
+    private char[][] cuadricula = new char[FILAS][COLUMNAS];
+
+
+
+    public Cuadricula() {
+        cuadricula = new char[FILAS][COLUMNAS];
+        inicializarCuadricula();
+    }
+
+    private void inicializarCuadricula() {
+        for (int i = 0; i < FILAS; i++) {
+            for (int j = 0; j < COLUMNAS; j++) {
+                cuadricula[i][j] = '%';
+            }
+        }
+    }
+
+    public void depositarBarco(int fila, int columna){
+        cuadricula[fila][columna] = 'B';
+
+    }
+
+    public boolean disparar(int fila, int columna){
+        boolean dispararBarco = false;
+        if(cuadricula[fila][columna] == 'B'){
+            cuadricula[fila][columna] = 'X';
+            dispararBarco = true;
+        }
+        else{
+            cuadricula[fila][columna] = 'O';
+            dispararBarco = false;
+        }
+        return dispararBarco;
+    }
+
+
+    public void visualizacion() {
+        System.out.println("  1 2 3 4 5 6 7 8 9 10 ");
+        for (int i = 0; i < FILAS; i++) {
+            System.out.print((char) ('A' + i) + " ");
+            for (int j = 0; j < COLUMNAS; j++) {
+                if (cuadricula[i][j] == 'B') {
+                    System.out.print("X ");
+                } else {
+                    System.out.print(cuadricula[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
 }
+
+
--- src/main/java/ejercicio4/Main.java
+++ src/main/java/ejercicio4/Main.java
@@ -1,4 +1,60 @@
 package ejercicio4;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner sn = new Scanner(System.in);
+    public static void main(String[] args) {
+        Cuadricula cuadriculaJugador = new Cuadricula();
+        Cuadricula cuadriculaMaquina = new Cuadricula();
+        System.out.println("Bienvenido al juego de la batalla naval");
+        System.out.println("Introduzca el barco que desea colocar: ");
+        System.out.println("Introduce la columna (entre 1 y 10):");
+        int colJugador = sn.nextInt();
+        System.out.println("Introduce la fila (entre 1 y 10):");
+        int filaJugador = sn.nextInt();
+        cuadriculaJugador.depositarBarco(filaJugador, colJugador);
+        System.out.println("¡Estupendo!, se ha añadido correctamente el barco");
+        System.out.println("Ahora espere a que la máquina coloque su barco");
+        int colMaquina = (int) (Math.random() * 10);
+        int filaMaquina = (int) (Math.random() * 10);
+        cuadriculaMaquina.depositarBarco(filaMaquina, colMaquina);
+        System.out.println("¡La máquina ha colocado su barco!");
+        System.out.println("Ahora empieza el juego");
+        try{
+            do{
+                System.out.println("Introduzca la columna donde desea disparar:");
+                int colDisparoJugador = sn.nextInt();
+                System.out.println("Introduzca la fila donde desea disparar:");
+                int filaDisparoJugador = sn.nextInt();
+                boolean barcoHundido = cuadriculaMaquina.disparar(filaDisparoJugador, colDisparoJugador);
+                cuadriculaMaquina.visualizacion();
+                if(barcoHundido){
+                    System.out.println("¡Has hundido el barco!");
+                    break;
+                }
+                else{
+                    System.out.println("¡Has fallado!");
+                }
+                System.out.println("Ahora le toca a la máquina");
+                int colDisparoMaquina = (int) (Math.random() * 10);
+                int filaDisparoMaquina = (int) (Math.random() * 10);
+                boolean barcoHundidoMaquina = cuadriculaJugador.disparar(filaDisparoMaquina, colDisparoMaquina);
+                cuadriculaJugador.visualizacion();
+                if(barcoHundidoMaquina){
+                    System.out.println("¡La máquina ha hundido tu barco!");
+                    break;
+                }
+                else{
+                    System.out.println("¡La máquina ha fallado!");
+                }
+            }while(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("¡Gracias por jugar!");
+
+
+    }
 }
